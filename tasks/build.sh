a1033f13f82914a41e38d664255e95a0ab3497ec
Joshua Carp <jm.carp@gmail.com>
2016-12-16 19:34:45 -0500

    Initial commit.

---
+++ tasks/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -u
+
+cat << EOF > credentials.json
+{
+  "username": "${AUTH_USERNAME}",
+  "password": "${AUTH_PASSWORD}",
+  "s3_config": {
+    "region": "${AWS_REGION}",
+    "aws_partition": "${AWS_PARTITION}",
+    "bucket_prefix": "${BUCKET_PREFIX}",
+    "iam_path": "${IAM_PATH}"
+  }
+}
+EOF
+
+cp -r broker-src/. broker-src-built
+
+jq -s '.[0] * .[1]' broker-src/config-sample.json credentials.json > \
+  broker-src-built/config.json
