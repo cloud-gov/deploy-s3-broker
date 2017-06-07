a805e0145efae91421c9588b22e85f7194d573ac
Joshua Carp <jm.carp@gmail.com>
2017-06-07 12:36:43 -0400

    Use instance profile instead of user credentials.

--- tasks/build.sh
+++ tasks/build.sh
@@ -20,5 +20,17 @@ EOF
 
 cp -r broker-src/. broker-src-built
 
+# Override upstream example manifest
+cat << EOF > broker-src-built/manifest.yml
+applications:
+- name: s3-broker
+  memory: 256M
+  disk_quota: 256M
+  buildpack: go_buildpack
+  command: s3-broker --config ./config.json --port \$PORT
+env:
+  GOVERSION: go1.7
+EOF
+
 jq -s '.[0] * .[1]' broker-config/config-template.json credentials.json > \
   broker-src-built/config.json
