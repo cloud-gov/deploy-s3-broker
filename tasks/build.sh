dad6462e4287e78b83519f008d9eab6a05806d46
Joshua Carp <jm.carp@gmail.com>
2017-07-03 10:20:20 -0400

    Switch to yaml config.

--- tasks/build.sh
+++ tasks/build.sh
@@ -3,19 +3,20 @@
 set -e
 set -u
 
-cat << EOF > credentials.json
-{
-  "username": "${AUTH_USERNAME}",
-  "password": "${AUTH_PASSWORD}",
-  "s3_config": {
-    "region": "${AWS_REGION}",
-    "aws_partition": "${AWS_PARTITION}",
-    "user_prefix": "${USER_PREFIX}",
-    "policy_prefix": "${POLICY_PREFIX}",
-    "bucket_prefix": "${BUCKET_PREFIX}",
-    "iam_path": "${IAM_PATH}"
-  }
-}
+cat << EOF > credentials.yml
+username: "${AUTH_USERNAME}"
+password: "${AUTH_PASSWORD}"
+s3_config:
+  region: "${AWS_REGION}"
+  aws_partition: "${AWS_PARTITION}"
+  user_prefix: "${USER_PREFIX}"
+  policy_prefix: "${POLICY_PREFIX}"
+  bucket_prefix: "${BUCKET_PREFIX}"
+  iam_path: "${IAM_PATH}"
+cf_config:
+  api_url: "${CF_API_URL}"
+  user: "${CF_USERNAME}"
+  password: "${CF_PASSWORD}"
 EOF
 
 cp -r broker-src/. broker-src-built
@@ -27,10 +28,10 @@ applications:
   memory: 256M
   disk_quota: 256M
   buildpack: go_buildpack
-  command: s3-broker --config ./config.json --port \$PORT
+  command: s3-broker --config ./config.yml --port \$PORT
 env:
-  GOVERSION: go1.7
+  GOVERSION: go1.8
 EOF
 
-jq -s '.[0] * .[1]' broker-config/config-template.json credentials.json > \
-  broker-src-built/config.json
+spruce merge broker-config/config-template.yml credentials.yml \
+  > broker-src-built/config.yml
