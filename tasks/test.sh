1f4776e45ad00d8b48f0a50f8f08b0c7e4e5dbaa
Mike Lloyd <mike.lloyd@gsa.gov>
2019-09-13 11:50:28 -0600

    fixing gopaths and updating images.
    Signed-off-by: Mike Lloyd <mike.lloyd@gsa.gov>

--- tasks/test.sh
+++ tasks/test.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-export GOPATH=$(pwd)/gopath
-export PATH=$PATH:$GOPATH/bin
+cd s3-broker
+go get -v ./...
 
-cd gopath/src/github.com/cloudfoundry-community/s3-broker
+go test -v ./...
 
-go get github.com/onsi/ginkgo/ginkgo
-go get github.com/onsi/gomega
-
-ginkgo -r
