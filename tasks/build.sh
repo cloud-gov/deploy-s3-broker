b2408ea821ffed1f0a1c5bebe90b0ac48e324b48
Mark Boyd <mark.boyd@gsa.gov>
2025-04-04 13:59:14 -0400

    Fix manifest build (#73)
    * refactor manifest preparation for deployments
    
    * change branch
    
    * refactor task inputs
    
    * fix file reference
    
    * fix dev deploy
    
    * update manifests
    
    * simplify pipeline to use credhub vars
    
    * change branch back to main

--- tasks/build.sh
+++ tasks/build.sh
@@ -22,32 +22,7 @@ EOF
 
 cp -r broker-src/. broker-src-built
 
-# Override upstream example manifest
-cat << EOF > broker-src-built/manifest.yml
-applications:
-- name: s3-broker
-  memory: 256M
-  disk_quota: 256M
-  buildpack: go_buildpack
-  command: s3-broker --config ./config.yml --port \$PORT
-env:
-  GOPACKAGENAME: github.com/cloud-gov/s3-broker
-EOF
-
-if [ -d "terraform-yaml" ]; then
-ACCESS_KEY_ID=$(grep 's3_broker_user_access_key_id_curr' "terraform-yaml/state.yml" | awk '{print $2}')
-SECRET_ACCESS_KEY=$(grep 's3_broker_user_secret_access_key_curr' "terraform-yaml/state.yml" | awk '{print $2}')
-
-cat << EOF > broker-src-built/vars.yml
-access_key_id: $ACCESS_KEY_ID
-secret_access_key: $SECRET_ACCESS_KEY
-EOF
-
-cat << EOF >> broker-src-built/manifest.yml
-  AWS_ACCESS_KEY: ((access_key_id))
-  AWS_SECRET_ACCESS_KEY: ((secret_access_key))  
-EOF
-fi
+cp broker-config/manifest*.yml broker-src-built
 
 # if a the config-template has the variable $INTERNAL_VPCE_ID
 # in it then replace with passed in envar value $INTERNAL_VPCE_ID
