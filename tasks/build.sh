be4d835fb5f59e8f48065d9bff9146b9aeb24629
Mark Boyd <mark.boyd@gsa.gov>
2025-04-04 12:13:21 -0400

    refactor manifest preparation for deployments

--- tasks/build.sh
+++ tasks/build.sh
@@ -20,32 +20,15 @@ cf_config:
   client_secret: "${CF_CLIENT_SECRET}"
 EOF
 
-cp -r broker-src/. broker-src-built
-
-# Override upstream example manifest
-cat << EOF > broker-src-built/manifest.yml
-applications:
-- name: s3-broker
-  memory: 256M
-  disk_quota: 256M
-  buildpack: go_buildpack
-  command: s3-broker --config ./config.yml --port \$PORT
-env:
-  GOPACKAGENAME: github.com/cloud-gov/s3-broker
-EOF
+cp -r broker-config/. broker-config-built
 
 if [ -d "terraform-yaml" ]; then
 ACCESS_KEY_ID=$(grep 's3_broker_user_access_key_id_curr' "terraform-yaml/state.yml" | awk '{print $2}')
 SECRET_ACCESS_KEY=$(grep 's3_broker_user_secret_access_key_curr' "terraform-yaml/state.yml" | awk '{print $2}')
 
-cat << EOF > broker-src-built/vars.yml
+cat << EOF > broker-config-built/vars.yml
 access_key_id: $ACCESS_KEY_ID
 secret_access_key: $SECRET_ACCESS_KEY
-EOF
-
-cat << EOF >> broker-src-built/manifest.yml
-  AWS_ACCESS_KEY: ((access_key_id))
-  AWS_SECRET_ACCESS_KEY: ((secret_access_key))  
 EOF
 fi
 
@@ -56,4 +39,4 @@ regex="s/\$INTERNAL_VPCE_ID/${INTERNAL_VPCE_ID:-}/"
 sed $regex broker-config/"${CONFIG_FILE_NAME}".yml > "${CONFIG_FILE_NAME}-rendered".yml
 
 spruce merge "${CONFIG_FILE_NAME}-rendered".yml credentials.yml \
-  > broker-src-built/config.yml
+  > broker-config-built/config.yml
